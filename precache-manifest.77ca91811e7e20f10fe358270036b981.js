3f045354c668544137db4be01508ad79515554de
UCA <>
2019-11-01 11:28:43 -0600

    Updates

---
+++ precache-manifest.77ca91811e7e20f10fe358270036b981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbd6f6b5a5191f42ec6ddec38c19e69",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "d27dd28d00de28ad888d",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "d27dd28d00de28ad888d",
+    "url": "/tictactoe/static/js/main.7b26a624.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
